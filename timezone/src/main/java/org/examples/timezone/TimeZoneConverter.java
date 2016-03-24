29d25d83552155999143cf15a30410cb01717410
bartosz <b.wieczorek@cctechnology.pl>
2016-03-24 21:43:04 +0100

    TimeZoneConverter stub

---
+++ timezone/src/main/java/org/examples/timezone/TimeZoneConverter.java
@@ -0,0 +1,9 @@
+package org.examples.timezone;
+import java.util.TimeZone;
+
+
+public class TimeZoneConverter {
+    public String convertDate(String date, TimeZone source, TimeZone destination) {
+        return null;
+    }
+}
