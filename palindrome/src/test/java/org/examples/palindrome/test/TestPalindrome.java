25f976cc630532f3e2b96a0b0aeb03b9410f6b4f
bartosz <b.wieczorek@cctechnology.pl>
2016-03-24 21:13:18 +0100

    More  palindrom tests added

--- palindrome/src/test/java/org/examples/palindrome/test/TestPalindrome.java
+++ palindrome/src/test/java/org/examples/palindrome/test/TestPalindrome.java
@@ -12,6 +12,11 @@ public class TestPalindrome {
 
     @Test
     public void phraseShouldBePalindrome() {
+        Assert.assertTrue(palindromeChecker.isPalindrome("anna"));
+    }
+
+    @Test
+    public void phraseShouldBePalindromeExtraCharacters() {
         Assert.assertTrue(palindromeChecker.isPalindrome("A new order began, a more Roman age bred Rowena."));
     }
 
@@ -19,4 +24,21 @@ public class TestPalindrome {
     public void phraseShouldNotBePalindrome() {
         Assert.assertFalse(palindromeChecker.isPalindrome("a more Roman age bred Rowena."));
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void phraseShouldThrowException() {
+        Assert.assertTrue(palindromeChecker.isPalindrome(null));
+    }
+
+    @Test
+    public void phraseShouldBePalindromeEmptyValue() {
+        Assert.assertTrue(palindromeChecker.isPalindrome(""));
+    }
+
+    @Test
+    public void phraseShouldBePalindromeSingleLetter() {
+        Assert.assertTrue(palindromeChecker.isPalindrome("A"));
+    }
+
+
 }
