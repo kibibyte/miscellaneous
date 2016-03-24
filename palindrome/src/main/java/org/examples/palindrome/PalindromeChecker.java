6b2d14d3d746f1e3eb475121fe33e31b5f8abaaf
bartosz <b.wieczorek@cctechnology.pl>
2016-03-24 21:12:15 +0100

    Check if coandidate string is palindrome

--- palindrome/src/main/java/org/examples/palindrome/PalindromeChecker.java
+++ palindrome/src/main/java/org/examples/palindrome/PalindromeChecker.java
@@ -1,7 +1,22 @@
 package org.examples.palindrome;
 
+
 public class PalindromeChecker {
-    public boolean isPalindrome(String value) {
+
+    public boolean isPalindrome(String candidate) {
+        if (candidate == null) {
+            throw new IllegalArgumentException();
+        }
+        return isCandidatePalindrome(candidate.toLowerCase().replaceAll("[^A-Za-z0-9]", ""));
+    }
+
+    private boolean isCandidatePalindrome(String candidate) {
+        if (candidate.length() == 0 || candidate.length() == 1) {
+            return true;
+        }
+        if (candidate.charAt(0) == candidate.charAt(candidate.length() - 1)) {
+            return isCandidatePalindrome(candidate.substring(1, candidate.length() - 1));
+        }
         return false;
     }
 }
