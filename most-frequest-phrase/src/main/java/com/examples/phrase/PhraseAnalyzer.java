4dfbd66376e6835220611f29175c9c56e6c07c73
bartosz <b.wieczorek@cctechnology.pl>
2016-03-26 16:46:26 +0100

    Test data changed

--- most-frequest-phrase/src/main/java/com/examples/phrase/PhraseAnalyzer.java
+++ most-frequest-phrase/src/main/java/com/examples/phrase/PhraseAnalyzer.java
@@ -17,7 +17,7 @@ public class PhraseAnalyzer {
     public void indexMostFrequentPhrases(int phraseLimit, int phraseOccurrencesLowerBound) throws IOException {
         FileWriter fw = new FileWriter(outputDirectoryPath + File.separator + "most_frequent_phrases.txt");
         fw.write("enterprise solutions | 5\n");
-        fw.write("videolicious | 4\n");
+        fw.write("video | 4\n");
         fw.write("poland | 3\n");
         fw.write("great | 2");
         fw.close();
